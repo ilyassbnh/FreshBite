f987878358d6ad28ca39486c43ad6c04191a9ea6
ilyassbnh <benhida.ilyass@gmail.com>
2025-11-05 14:42:17 +0100

    home page and navbar with responsivity

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSideBar() {
+    const sideBar = document.querySelector('.sideBar')
+    sideBar.style.display='block'
+} 
+function hideSideBar(){
+    const sideBar = document.querySelector('.sideBar')
+    sideBar.style.display="none"
+}
